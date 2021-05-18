7cba277bfd9f749c006b1982c5cf11c8741b1c0e
José Gomes <zemiguelgomes99@gmail.com>
2021-05-18 01:43:44 +0100

    Integrate locked door in message system

--- Damian's Delusion/Assets/Scripts/UI/MessageManager.cs
+++ Damian's Delusion/Assets/Scripts/UI/MessageManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MessageManager : MonoBehaviour
 {
 
-    private static MessageManager instance;
+    public static MessageManager instance;
     public GameObject messagePrefab;
 
     public List<HUD_Message> messageSlots;
