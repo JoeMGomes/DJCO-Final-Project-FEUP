550cb6f663a2bf13b63c10add8e8f15331b8dc1b
joaorenatopinto <jrcp210@gmail.com>
2021-06-02 00:00:50 +0100

    Added alcohol dialogue, safe gives alcohol too

---
+++ Damian's Delusion/Assets/Scripts/Dialogue/CallBacks/Patient3.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patient3 : IDialogueCallBack
+{
+
+    public Key key;
+
+    public override IEnumerator CallBack_4()
+    {
+        MessageManager.instance.InsertMessage("Alcohol lost");
+        InventoryController.instance.Add(key);
+        MessageManager.instance.InsertMessage("Key Received");
+        DialogueManager.instance.EndDialogue();
+        //Enumerator return is mandatory to enable more complex callbacks
+        yield return new WaitForSeconds(0.01f);
+    }
+}
