a6dd89d82a2bc19c3a9c6ae841b0584799e03636
José Gomes <zemiguelgomes99@gmail.com>
2021-06-14 15:39:35 +0100

    removed garbage

--- Damian's Delusion/Assets/Scripts/Items/Knife.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu (fileName = "new Item", menuName = "Items/Knife")]
-public class Knife : Item
-{
-
-}
