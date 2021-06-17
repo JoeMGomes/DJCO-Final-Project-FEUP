e54211f76470eeb2686293b53037ea4ceb17464b
José Gomes <zemiguelgomes99@gmail.com>
2021-06-17 14:49:16 +0100

    Add wall and floor and new building

--- Damians Delusion/Assets/Scripts/Player/playerMovement.cs
+++ Damians Delusion/Assets/Scripts/Player/playerMovement.cs
@@ -97,6 +97,7 @@ public class playerMovement : MonoBehaviour
                     step1.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE state);
                     if (state == FMOD.Studio.PLAYBACK_STATE.STOPPED)
                     {
+                        Debug.Log("1");
                         step2.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step3.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step1.start();
@@ -109,6 +110,7 @@ public class playerMovement : MonoBehaviour
                     step2.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE state);
                     if (state == FMOD.Studio.PLAYBACK_STATE.STOPPED)
                     {
+                        Debug.Log("2");
                         step1.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step3.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step2.start();
@@ -120,6 +122,7 @@ public class playerMovement : MonoBehaviour
                     step3.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE state);
                     if (state == FMOD.Studio.PLAYBACK_STATE.STOPPED)
                     {
+                        Debug.Log("3");
                         step2.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step1.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
                         step3.start();
